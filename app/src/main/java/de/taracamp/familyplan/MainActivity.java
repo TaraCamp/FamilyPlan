b3bce1a657c5eded55911cd2280b43ade3cfd552
Wladimir Tarasov <wladimir.tarasov@cn-consult.eu>
2017-03-07 14:02:04 +0100

    add TaskActivity and accountActivity

--- app/src/main/java/de/taracamp/familyplan/MainActivity.java
+++ app/src/main/java/de/taracamp/familyplan/MainActivity.java
@@ -14,16 +14,22 @@ import com.google.firebase.auth.FirebaseUser;
 import de.taracamp.familyplan.Login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     private static final String TAG = "MainActivity";
     private static final String LOG_AUTH_FIREBASE = "LOG_AUTH_FIREBASE";
 
-    private RelativeLayout relativeLayoutLogout;
+    private RelativeLayout button_menu_task;
+    private RelativeLayout button_menu_account;
+    private RelativeLayout button_menu_3;
+    private RelativeLayout button_menu_4;
+    private RelativeLayout button_menu_5;
+    private RelativeLayout button_menu_logout;
+
+        /* Firebase */
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
-    private void initialize(){
+    private void initFirebase(){
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -36,9 +42,53 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+    }
 
-        relativeLayoutLogout = (RelativeLayout)findViewById(R.id.menu_button_logout);
-        relativeLayoutLogout.setOnClickListener(new View.OnClickListener() {
+    private void initialize(){
+
+        button_menu_task = (RelativeLayout)findViewById(R.id.button_menu_task);
+        button_menu_task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent taskIntent = new Intent(getApplicationContext(),TaskActivity.class);
+                startActivity(taskIntent);
+            }
+        });
+
+        button_menu_account = (RelativeLayout)findViewById(R.id.button_menu_account);
+        button_menu_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button_menu_3 = (RelativeLayout)findViewById(R.id.button_menu_3);
+        button_menu_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button_menu_4 = (RelativeLayout)findViewById(R.id.button_menu_4);
+        button_menu_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button_menu_5 = (RelativeLayout)findViewById(R.id.button_menu_5);
+        button_menu_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button_menu_logout = (RelativeLayout)findViewById(R.id.button_menu_logout);
+        button_menu_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
@@ -54,7 +104,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       initialize();
+        initFirebase();
+        initialize();
 
     }
 
